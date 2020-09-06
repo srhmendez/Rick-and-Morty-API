aa5ec0493c83e22aa05d79aaccaf7923b325eb17
Sarah <srhmendez@gmail.com>
2020-09-06 14:37:55 -0600

    adding all characters button

--- main.js
+++ main.js
@@ -99,6 +99,20 @@ alienCharButton.addEventListener('click', function() {
     })
 })
 
+const allCharButton = document.querySelector('.allcharacterbutton')
+allCharButton.addEventListener('click', function() {
+    deleteCards();
+    getAPIData(`https://rickandmortyapi.com/api/character/?page=1&2`)
+    .then(data => {
+        for (const allCharacters of data.results) {
+        getAPIData(allCharacters.url)
+        .then(characterdata => {
+            populateDOM(characterdata)
+            })
+        }
+    })
+})
+
 function deleteCards() {
     while (mainArea.firstChild) {
       mainArea.removeChild(mainArea.firstChild);
